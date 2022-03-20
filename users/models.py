7478729d66ce6fbc6dbe5ed1ad03172fa9f9b9a3
Dave <daveg7lee@gmail.com>
2022-03-20 14:54:28 +0900

    Finishing User Model

--- users/models.py
+++ users/models.py
@@ -16,6 +16,22 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         default=GENDER_MALE,
@@ -25,3 +41,19 @@ class User(AbstractUser):
         blank=True,
     )
     bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        default=LANGUAGE_KOREAN,
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        null=True,
+        blank=True,
+    )
+    currency = models.CharField(
+        default=CURRENCY_KRW,
+        choices=CURRENCY_CHOICES,
+        max_length=3,
+        null=True,
+        blank=True,
+    )
+    superhost = models.BooleanField(default=False)
