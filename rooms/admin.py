4cc33ed161d2f88afc326039820ec7cbdf8a91fa
Dave <daveg7lee@gmail.com>
2022-03-29 23:39:08 +0900

    Meta Class and Photos Model

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
+@admin.register(models.RoomType, models.Amenity, models.HouseRule, models.Facility)
 class ItemAdmin(admin.ModelAdmin):
+    """Item Admin Definition"""
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """Room Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
     pass
